e343c523ea98fb5cd88f4bca601b3740934e62e4
Rodrigo Bresan <rodrigo.bresan@cleevio.cz>
2017-10-20 14:23:12 -0200

    Remote module structure added (src and test folders)

---
+++ remote/src/main/java/com/rodrigobresan/remote/myClass.java
@@ -0,0 +1,4 @@
+package com.rodrigobresan.remote;
+
+public class myClass {
+}
---
+++ remote/src/test/java/com/rodrigobresan/remote/SampleTest.java
@@ -0,0 +1,22 @@
+package com.rodrigobresan.remote;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by rodrigobresan on 20/10/17.
+ * <p>
+ * In case of any questions, feel free to ask me
+ * <p>
+ * E-mail: rcbresan@gmail.com
+ * Slack: bresan
+ */
+
+public class SampleTest {
+
+    @Test
+    public void sampleTest() {
+        assertEquals(4, 2 + 2);
+    }
+}
