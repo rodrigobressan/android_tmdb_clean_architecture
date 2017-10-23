db3582f2d0d00cac1321ce0488c3fa0fc98ef67b
Rodrigo Bresan <rodrigo.bresan@cleevio.cz>
2017-10-23 16:03:56 -0200

    Domain layer major implementation

---
+++ domain/src/main/java/com/rodrigobresan/domain/interactor/GetMovies.kt
@@ -0,0 +1,20 @@
+package com.rodrigobresan.domain.interactor
+
+import com.rodrigobresan.domain.executor.PostExecutionThread
+import com.rodrigobresan.domain.executor.ThreadExecutor
+import com.rodrigobresan.domain.model.Movie
+import com.rodrigobresan.domain.repository.MovieRepository
+import io.reactivex.Single
+import javax.inject.Inject
+
+open class GetMovies @Inject constructor(val movieRepository: MovieRepository,
+                                         threadExecutor: ThreadExecutor,
+                                         postExecutionThread: PostExecutionThread) :
+
+        SingleUseCase<List<Movie>, Void?>(threadExecutor, postExecutionThread) {
+
+    public override fun buildUseCaseObservable(params: Void?): Single<List<Movie>> {
+        return movieRepository.getMovies()
+    }
+
+}
---
+++ domain/src/test/java/com/rodrigobresan/domain/usecase/GetMoviesTest.kt
@@ -0,0 +1,61 @@
+package com.rodrigobresan.domain.usecase
+
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.whenever
+import com.rodrigobresan.domain.executor.PostExecutionThread
+import com.rodrigobresan.domain.executor.ThreadExecutor
+import com.rodrigobresan.domain.interactor.GetMovies
+import com.rodrigobresan.domain.model.Movie
+import com.rodrigobresan.domain.repository.MovieRepository
+import com.rodrigobresan.domain.test.factory.MovieFactory
+import io.reactivex.Single
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class GetMoviesTest {
+
+    private lateinit var getMovies: GetMovies
+    private lateinit var movieRepository: MovieRepository
+    private lateinit var threadExecutor: ThreadExecutor
+    private lateinit var postExecutionThread: PostExecutionThread
+
+    @Before
+    fun setUp() {
+        movieRepository = mock()
+        threadExecutor = mock()
+        postExecutionThread = mock()
+        getMovies = GetMovies(movieRepository, threadExecutor, postExecutionThread)
+    }
+
+    @Test
+    fun buildCaseObservableCallsMoviesRepository() {
+        getMovies.buildUseCaseObservable(null)
+        verify(movieRepository).getMovies()
+    }
+
+    @Test
+    fun buildUseCaseObservableCompletes() {
+        stubMovieRepositoryGetMovies(Single.just(MovieFactory.makeMovieList(2)))
+
+        val testObservable = getMovies.buildUseCaseObservable(null).test()
+        testObservable.assertComplete()
+    }
+
+    @Test
+    fun buildUseCaseObservableReturnsData() {
+        val movies = MovieFactory.makeMovieList(2)
+        stubMovieRepositoryGetMovies(Single.just(movies))
+
+        val testObserver = getMovies.buildUseCaseObservable(null).test()
+        testObserver.assertResult(movies)
+    }
+
+    private fun stubMovieRepositoryGetMovies(singleMovieList: Single<List<Movie>>?) {
+        whenever(movieRepository.getMovies())
+                .thenReturn(singleMovieList)
+    }
+}
