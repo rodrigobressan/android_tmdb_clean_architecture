d2768a3e5ca4e0349524411b8290b29bf3afd1bb
Rodrigo Bresan <rodrigo.bresan@cleevio.cz>
2017-10-22 23:26:44 -0200

    Single use case added

---
+++ domain/src/main/java/com/rodrigobresan/domain/interactor/SingleUseCase.kt
@@ -0,0 +1,37 @@
+package com.rodrigobresan.domain.interactor
+
+import com.rodrigobresan.domain.executor.PostExecutionThread
+import com.rodrigobresan.domain.executor.ThreadExecutor
+import io.reactivex.Single
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
+
+abstract class SingleUseCase<T, in Params> constructor(
+        private val threadExecutor: ThreadExecutor,
+        private val postExecutionThread: PostExecutionThread) {
+
+
+    private val disposables = CompositeDisposable()
+
+    protected abstract fun buildUseCaseObservable(params: Params? = null): Single<T>
+
+    open fun execute(singleObserver: DisposableSingleObserver<T>, params: Params? = null) {
+        val single = this.buildUseCaseObservable(params)
+                .subscribeOn(Schedulers.from(threadExecutor))
+                .observeOn(postExecutionThread.scheduler) as Single<T>
+
+        addDisposable(single.subscribeWith(singleObserver))
+    }
+
+    fun dispose() {
+        if (!disposables.isDisposed) {
+            disposables.dispose()
+        }
+    }
+
+    private fun addDisposable(disposable: Disposable) {
+        disposables.add(disposable)
+    }
+}
