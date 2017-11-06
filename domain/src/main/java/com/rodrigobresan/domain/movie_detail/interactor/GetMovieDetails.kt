a98e312cf30689ce46f15755e716b31813dfa94d
Rodrigo Bresan <rcbresan@gmail.com>
2017-11-06 17:37:16 -0200

    Movie detail impl on domain layer

---
+++ domain/src/main/java/com/rodrigobresan/domain/movie_detail/interactor/GetMovieDetails.kt
@@ -0,0 +1,20 @@
+package com.rodrigobresan.domain.movie_detail.interactor
+
+import com.rodrigobresan.domain.base.executor.PostExecutionThread
+import com.rodrigobresan.domain.base.executor.ThreadExecutor
+import com.rodrigobresan.domain.interactor.SingleUseCase
+import com.rodrigobresan.domain.movie_detail.model.MovieDetail
+import com.rodrigobresan.domain.movie_detail.repository.MovieDetailRepository
+import io.reactivex.Single
+import javax.inject.Inject
+
+open class GetMovieDetails @Inject constructor(val movieDetailRepository: MovieDetailRepository,
+                                               threadExecutor: ThreadExecutor,
+                                               postExecutionThread: PostExecutionThread)
+    : SingleUseCase<MovieDetail, Long>(threadExecutor, postExecutionThread) {
+
+    public override fun buildUseCaseObservable(movieId: Long?): Single<MovieDetail> {
+        return movieDetailRepository.getMovieDetails(movieId!!)
+    }
+
+}
