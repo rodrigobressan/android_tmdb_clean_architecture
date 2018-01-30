1a553a0888c2a57bbfbc645b74e8143fcaf1b5b7
Rodrigo Bresan <rcbresan@gmail.com>
2018-01-30 19:07:34 -0200

    Review remote data store unit tests

--- data/src/test/java/com/rodrigobresan/data/review/source/ReviewDataStoreFactoryTest.kt
+++ data/src/test/java/com/rodrigobresan/data/review/source/ReviewDataStoreFactoryTest.kt
@@ -3,101 +3,59 @@ package com.rodrigobresan.data.review.source
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.whenever
 import com.rodrigobresan.data.connection.ConnectionStatus
-import com.rodrigobresan.data.movie.sources.data_store.MovieDataStoreFactory
-import com.rodrigobresan.data.movie.sources.data_store.local.MovieCache
-import com.rodrigobresan.data.movie.sources.data_store.local.MovieCacheDataStore
-import com.rodrigobresan.data.movie.sources.data_store.remote.MovieRemoteDataStore
+import com.rodrigobresan.data.movie.sources.data_store.ReviewDataStoreFactory
+import com.rodrigobresan.data.review.sources.data_store.local.ReviewCache
+import com.rodrigobresan.data.review.sources.data_store.local.ReviewCacheDataStore
+import com.rodrigobresan.data.review.sources.data_store.local.ReviewRemoteDataStore
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
-/**
- * Class for testing MovieDataStoreFactory class
- */
 @RunWith(JUnit4::class)
 class ReviewDataStoreFactoryTest {
 
-    private lateinit var movieDataStoreFactory: MovieDataStoreFactory
+    private lateinit var reviewDataStoreFactory: ReviewDataStoreFactory
 
-    private lateinit var movieCache: MovieCache
-    private lateinit var movieCacheDataStore: MovieCacheDataStore
-    private lateinit var movieRemoteDataStore: MovieRemoteDataStore
+    private lateinit var reviewCache: ReviewCache
+    private lateinit var reviewCacheDataStore: ReviewCacheDataStore
+    private lateinit var reviewRemoteDataStore: ReviewRemoteDataStore
     private lateinit var connectionStatus: ConnectionStatus
 
     @Before
     fun setUp() {
-        movieCache = mock()
-        movieCacheDataStore = mock()
-        movieRemoteDataStore = mock()
+        reviewCache = mock()
+        reviewCacheDataStore = mock()
+        reviewRemoteDataStore = mock()
         connectionStatus = mock()
 
-        movieDataStoreFactory = MovieDataStoreFactory(connectionStatus, movieCache,
-                movieCacheDataStore, movieRemoteDataStore)
+        reviewDataStoreFactory = ReviewDataStoreFactory(connectionStatus, reviewRemoteDataStore,
+                reviewCacheDataStore)
     }
 
     @Test
     fun retrieveDataStoreWhenNoConnectionReturnsCachedDataStore() {
         stubIsOffline(true)
 
-        val dataStore = movieDataStoreFactory.retrieveDataStore()
-        assert(dataStore is MovieCacheDataStore)
-    }
-
-
-    @Test
-    fun retrieveDataStoreWhenNotCachedReturnsRemoteDataStore() {
-        stubIsOffline(false)
-        stubMovieIsCached(false)
-
-        val dataStore = movieDataStoreFactory.retrieveDataStore()
-        assert(dataStore is MovieRemoteDataStore)
-    }
-
-    @Test
-    fun retrieveDataStoreWhenCachedExpiredReturnsRemoteDataStore() {
-        stubIsOffline(false)
-        stubMovieIsCached(false)
-        stubMovieIsExpired(true)
-
-        val dataStore = movieDataStoreFactory.retrieveDataStore()
-        assert(dataStore is MovieRemoteDataStore)
-    }
-
-    @Test
-    fun retrieveDataStoreWhenCacheExistsAndNotExpiredReturnsCachedDataStore() {
-        stubMovieIsCached(true)
-        stubMovieIsExpired(false)
-
-        val dataStore = movieDataStoreFactory.retrieveDataStore()
-        assert(dataStore is MovieCacheDataStore)
+        val dataStore = reviewDataStoreFactory.retrieveDataStore()
+        assert(dataStore is ReviewCacheDataStore)
     }
 
     @Test
     fun retrieveRemoteDataStoreReturnsRemoteDataStore() {
-        val movieDataStore = movieDataStoreFactory.retrieveRemoteDataStore()
-        assert(movieDataStore is MovieRemoteDataStore)
+        val ReviewDataStore = reviewDataStoreFactory.retrieveRemoteDataStore()
+        assert(ReviewDataStore is ReviewRemoteDataStore)
     }
 
     @Test
     fun retrieveCachedDataStoreReturnsCachedDataStore() {
-        val movieDataStore = movieDataStoreFactory.retrieveCachedDataStore()
-        assert(movieDataStore is MovieCacheDataStore)
+        val ReviewDataStore = reviewDataStoreFactory.retrieveCachedDataStore()
+        assert(ReviewDataStore is ReviewCacheDataStore)
     }
 
     private fun stubIsOffline(isOffline: Boolean) {
         whenever(connectionStatus.isOffline())
                 .thenReturn(isOffline)
-    }
-
-    private fun stubMovieIsCached(isCached: Boolean) {
-        whenever(movieCache.isCached())
-                .thenReturn(isCached)
-    }
-
-    private fun stubMovieIsExpired(isExpired: Boolean) {
-        whenever(movieCache.isExpired())
-                .thenReturn(isExpired)
     }
 
 }
