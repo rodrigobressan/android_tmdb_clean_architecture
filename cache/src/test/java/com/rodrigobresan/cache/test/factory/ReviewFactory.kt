667af6f759e52f5f085250227392e6f217bc0e68
Rodrigo Bresan <rcbresan@gmail.com>
2018-01-30 15:38:08 -0200

    Review entity read and write tests with Room

--- cache/src/test/java/com/rodrigobresan/cache/test/factory/ReviewFactory.kt
+++ cache/src/test/java/com/rodrigobresan/cache/test/factory/ReviewFactory.kt
@@ -10,6 +10,15 @@ class ReviewFactory {
 
     companion object Factory {
 
+        fun makeReviewCachedList(size: Int) : List<ReviewCached> {
+            var reviewList = arrayListOf<ReviewCached>()
+
+            (1..size).forEach {
+                reviewList.add(ReviewCached(0, "author", "content", "url"))
+            }
+
+            return reviewList
+        }
         fun makeReviewCached(): ReviewCached {
             return ReviewCached(DataFactory.randomLong(), DataFactory.randomUuid(),
                     DataFactory.randomUuid(), DataFactory.randomUuid())
