14c77770b3d5fb552ddc7e26f36bd42db5a94915
Rodrigo Bresan <rcbresan@gmail.com>
2018-02-14 20:32:33 -0200

    Add tv project into CI

--- scripts/ci/after_script.sh
+++ scripts/ci/after_script.sh
@@ -1,6 +1,13 @@
 #!/bin/bash -xe
 
+# Upload mobile app to crashlytics
 ./gradlew :mobile:crashlyticsUploadDistributionDemoDebug
 ./gradlew :mobile:crashlyticsUploadDistributionProDebug
 ./gradlew :mobile:crashlyticsUploadDistributionDemoQa
 ./gradlew :mobile:crashlyticsUploadDistributionProQa
+
+# Upload tv app to crashlytics
+./gradlew :tv:crashlyticsUploadDistributionDemoDebug
+./gradlew :tv:crashlyticsUploadDistributionProDebug
+./gradlew :tv:crashlyticsUploadDistributionDemoQa
+./gradlew :tv:crashlyticsUploadDistributionProQa
